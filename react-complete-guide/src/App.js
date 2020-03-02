aafb392180565d4fa43698fd3827ab332adfbd87
fernando737 <fernando.perezdp1@gmail.com>
2020-03-02 19:52:30 +0000

    Initial Commit

---
+++ react-complete-guide/src/App.js
@@ -0,0 +1,14 @@
+import React, { component } from 'react';
+import './App.css';
+
+class App extends component {
+    render() {
+    return (
+    <div className="App">
+        <h1>Hi im a react app</h1>
+    </div>
+        );
+    }   
+}
+
+export default App;
