03045dc2a0363749cbabea329cbdb8ad602d0b3f
fernando737 <fernando.perezdp1@gmail.com>
2020-03-14 21:51:09 +0000

    First commit

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,14 +1,29 @@
-import React, { component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
-class App extends component {
-    render() {
+
+const app = props => {
+    const [ personState, setPersonsState] = useState({
+        persons: [
+            { name: "Max", age: "28" },
+            { name: "Holi", age: "29" },
+            { name: "Holi2", age: "1" },
+        ],
+        otherstate: "some other value"
+    });
+
+
     return (
-    <div className="App">
-        <h1>Hi im a react app</h1>
-    </div>
-        );
-    }   
+            <div className="App">
+            <h1>Hi im a react app</h1>
+            <p>This is really working!!!</p>
+            <button onClick={this.switchNameHandler}>Boton</button>
+            <Person name={personState.persons[0].name} age={personState.persons[0].age} />
+            <Person name={personState.persons[1].name} age={personState.persons[1].age} />
+            <Person name={personState.persons[2].name} age={personState.persons[2].age} >My Hobbies: Racing</Person>
+            </div>
+    );
+    
 }
-
-export default App;
+export default app;
